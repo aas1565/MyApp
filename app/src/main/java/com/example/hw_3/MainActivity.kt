fe1df30623d8c7d126113ebf07f83b2b59aed49f
aas1565 <aas1565@yandex.ru>
2025-11-03 16:33:58 +0200

    practice-6

--- app/src/main/java/com/example/hw_3/MainActivity.kt
+++ app/src/main/java/com/example/hw_3/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.hw_3
 
 import BottomBar
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,6 +20,21 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.example.hw_3.viewmodel.FootballViewModel
 
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainApp() // Ваша существующая Composable функция
+                }
+            }
+        }
+    }
+}
 
 @Composable
 fun MainApp() {
